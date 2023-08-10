ca2779326fb48613779e7f4cfb180cca97492d1a
ahurara <hurrerahchamp57@gmail.com>
2023-08-10 15:08:23 +0500

    updated the css code and improved data retreval

--- src/DataPage.jsx
+++ src/DataPage.jsx
@@ -1,81 +1,47 @@
 import React from "react";
+import './index.css';
 
 const DataPage=(props)=>{
 
-    const tableContainer = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: '50px',
-        overflowX: 'auto', 
-        };
     
-      const tableStyle = {
-        borderCollapse: 'collapse', // Collapse table borders
-        border: 'none', // Add a border around the table
-        width:'800px',
-       
-      };
-    
-      const cellStyle = {
-        padding: '0px',
-        border: 'none',
-      };
-
-      const p={
-        color:'white',
-        fontSize:'25px'
-      }
-
-      const pd={
-        color:'white',
-        fontSize:'25px',
-        fontWeight:'bold'
-      }
-
-      const temp={
-        color:'white',
-        fontSize:'70px',
-        fontWeight:'bold'
-      }
     return(
         <>
-            <div style={tableContainer} className="table-responsive overflow-auto">
-            <table style={tableStyle} className="table ">
+            <div  className="table-responsive overflow-auto tableContainer">
+            <table  className="table tableStyle">
               <tbody>
                 <tr>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={pd}>{`${props.country},${props.city}`}</p>
+                  <td  className="bg-transparent cellStyle">
+                    <p className="pd">{`${props.country},${props.city}`}</p>
                   </td>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={p}>WIND</p>
+                  <td  className="bg-transparent cellStyle">
+                    <p className="p">WIND</p>
                   </td>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={pd}>{`${props.wind} Km/h`}</p>
+                  <td className="bg-transparent cellStyle">
+                    <p className="pd">{`${props.wind} Km/h`}</p>
                   </td>
                 </tr>
 
 
                 <tr>
-                  <td style={cellStyle} rowSpan={2} className="bg-transparent">
-                    <p style={temp}>{`${props.temp}`}<sup><sup>.</sup>c</sup></p>
+                  <td rowSpan={2} className="bg-transparent cellStyle">
+                    <p className="temp">{`${props.temp}`}<sup><sup>.</sup>c</sup></p>
                   </td>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={p}>PRESSURE</p>
+                  <td  className="bg-transparent cellStyle">
+                    <p className="p">PRESSURE</p>
                   </td>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={pd}>{`${props.pressure} hPa`}</p>
+                  <td className="bg-transparent cellStyle">
+                    <p className="pd">{`${props.pressure} hPa`}</p>
                   </td>
                 </tr>
 
 
                 <tr>
                  
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={p}>HUMIDITY</p>
+                  <td  className="bg-transparent cellStyle">
+                    <p className="p">HUMIDITY</p>
                   </td>
-                  <td style={cellStyle} className="bg-transparent">
-                    <p style={pd}>{`${props.humidity}%`}</p>
+                  <td  className="bg-transparent cellStyle">
+                    <p className="pd">{`${props.humidity}%`}</p>
                   </td>
                 </tr>
                 
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,76 +1,11 @@
 import React, { useState,useEffect } from "react";
-import FrontImg from "./asset/elizeu-dias-RN6ts8IZ4_0-unsplash.jpg";
 import DataPage from "./DataPage";
+import "./index.css"
+
 const MainPage=()=>{
     const [inputText, setInputText]=useState('');
     const [savedText, setSavedText]=useState('');
-
-
-    const mainDiv={
-        position: 'fixed',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        backgroundImage: `url(${FrontImg})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        
-    }
-
-    const overlay={
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(77, 173, 221, 0.8)',
-    }
-
-    const mainHeading={
-        textAlign: "center",
-        paddingTop:"100px",
-        color :"white",
-        fontWeight :'bold',
-        fontSize:'60px'
-    }
-
-    const p={
-        textAlign: "center",
-        color : 'white',
-        fontSize:'20px',
-
-    }
-
-    const input={
-        textAlign:'center',
-        paddingTop:'20px'
-    }
-
-    const inputfield={
-        border:'none',
-        outline:'noen',
-        width:'500px',
-        height:'60px',
-        borderRadius:'10px',
-        borderTopRightRadius:'0px',
-        borderBottomRightRadius:'0px',
-    }
-
-    const button={
-        height:'60px',
-        width:'100px',
-        borderRadius:'10px',
-        borderTopLeftRadius:'0px',
-        borderBottomLeftRadius:'0px',
-        border:'none',
-        outline:'none',
-        color:'black',
-        fontWeight:'bold',
-        backgroundColor:'rgba(218, 251, 8, 0.8)',
-    }
-
-   
+    const [isLoading, setIsLoading]=useState(false);
       
       const handleInputChane=(e)=>{
         setInputText(e.target.value)
@@ -90,8 +25,12 @@ const MainPage=()=>{
    
         useEffect(
             ()=>{
-                if(savedText !='')
+                if(savedText !== '')
                 {
+                setIsLoading(true);
+                setTimeout(()=>{
+                   
+                   
                 fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${savedText}`)
             .then((response)=>{
                
@@ -106,11 +45,15 @@ const MainPage=()=>{
                 //console.log(apiData);
                 setpost(apiData);
                
+            }).finally(()=>{
+                setIsLoading(false);
             })
-           }
+           
+                },2000)
+                
             }
             
-            ,[savedText]);
+        },[savedText]);
     
             useEffect(()=>{
                
@@ -124,37 +67,48 @@ const MainPage=()=>{
     return(
         <>
            
-           <div style={mainDiv} className="container-fluid" >
+           <div  className="container-fluid mainDiv" >
            <div className="row">
             <div className="col-md-12">
-            <div style={overlay}>
-                <h1 style={mainHeading}>Simple Weather App</h1>
+            <div  className="overlay">
+                <h1 className="mainHeading">Simple Weather App</h1>
                 <span>
-                <p style={p}>{savedText  ?   post && post.current.condition.text  : 'based on weatherAPI'}   {post && (
-                <img src={post.current.condition.icon} />
+                <p className="p">{savedText  ?   post && post.current.condition.text  : 'based on weatherAPI'}   {post && (
+                <img src={post.current.condition.icon} alt=""/>
                 )}</p>
               
                 </span>
                 <br/>
-                <div style={input}>
-                <input type="text" style={inputfield} placeholder="  Enter the city name" onChange={handleInputChane} value={inputText}/>
-                <button style={button} onClick={handleSaveChanges}>CHECK</button>
+                <div className="input">
+                <input type="text" className="inputfield" placeholder="  Enter the city name" onChange={handleInputChane} value={inputText}/>
+                <button className="button" onClick={handleSaveChanges}>CHECK</button>
                 </div>
                 <br/>
                 {
                     //if theres the data run the following
                 }
                {
-                post &&(
-               <DataPage
+
+                isLoading ?
+                (
+                <div className="loader-container">
+                <div className="loader"></div>
+                </div>
+               )
+                
+                :
+                (
+                post &&
+                <DataPage
                  country={post.location.country}
                  city={post.location.name}
                  wind={post.current.wind_kph}
                  pressure={post.current.pressure_mb}
                  humidity={post.current.humidity}
                  temp={post.current.temp_c}
-                 />
-               )}
+                 />)
+                
+               }
             </div>
            </div>
            </div>
@@ -164,3 +118,4 @@ const MainPage=()=>{
 }
 
 export default MainPage;
+
